ef9be050c770cf8b03adde32ba70758dfe68be56
Daniel Perez <dperez@soaint.com>
2022-03-17 10:32:46 -0400

    migrar codigo a EC6

--- dbserver.js
+++ dbserver.js
@@ -1,13 +1,17 @@
-const express = require('express');
+import express from 'express';
+import mysql from 'mysql';
+import bcrypt from 'bcrypt';
+import generateAccessToken from './generateAccessToken.js';
+import dotenv from 'dotenv';
+
 const app = express();
-const mysql = require('mysql');
-const bcrypt = require('bcrypt');
-require('dotenv').config();
+dotenv.config();
 
 const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_DATABASE = process.env.DB_DATABASE;
 const DB_PORT = process.env.DB_PORT;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 const port = process.env.PORT;
 
@@ -15,6 +19,7 @@ const db = mysql.createPool({
     connectionLimit: 100,
     host: DB_HOST,
     user: DB_USER,
+    //password: DB_PASSWORD;
     database: DB_DATABASE,
     port: DB_PORT
 });
@@ -30,8 +35,9 @@ app.listen(port,
 
 app.use(express.json());
 
+//CREAR USUARIO
 app.post('/createUser', async (req, res) => {
-    const user = req.body.name;
+    const user = req.body.user;
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
     db.getConnection(async (err, connection) => {
@@ -45,12 +51,12 @@ app.post('/createUser', async (req, res) => {
 
         await connection.query(search_query, async (err, result) => {
             if (err) throw err;
-            console.log('------> Search Result');
+            console.log('------> Resultados de la Búsqueda');
             console.log(result.length);
 
             if (result.length != 0) {
                 connection.release();
-                console.log('------> User already exists');
+                console.log(`------> Este nombre de usuario ya existe: ${user}`);
                 res.sendStatus(409);
             }
             else {
@@ -58,7 +64,7 @@ app.post('/createUser', async (req, res) => {
                     connection.release();
 
                     if (err) throw err;
-                    console.log('------> Created new user');
+                    console.log('------> Nuevo usuario creado');
                     console.log(result.insertId);
                     res.sendStatus(201);
                 });
@@ -67,3 +73,40 @@ app.post('/createUser', async (req, res) => {
     });
 });
 
+//LOGIN (Autenticar usuario)
+app.post("/login", (req, res) => {
+    const user = req.body.user;
+    const password = req.body.password;
+
+    db.getConnection(async (err, connection) => {
+        if (err) throw (err);
+        const sqlSearch = "Select * from userTable where user = ?";
+        const search_query = mysql.format(sqlSearch, [user]);
+
+        await connection.query(search_query, async (err, result) => {
+            connection.release();
+
+            if (err) throw (err);
+            if (result.length == 0) {
+                console.log(`--------> Este usuario no existe: ${user}`);
+                res.statusCode = 404;
+                res.send('Usuario no encontrado');
+            }
+            else {
+                const hashedPassword = result[0].password;
+                //obtener contraseña cifrada
+                if (await bcrypt.compare(password, hashedPassword)) {
+                    console.log("---------> Usuario autenticado satisfactoriamente");
+                    console.log("---------> Generando token de acceso");
+                    const token = generateAccessToken({ user: user });
+                    console.log(token);
+                    res.json({ accessToken: token });
+                }
+                else {
+                    console.log("---------> Contraseña incorrecta");
+                    res.send("Contraseña incorrecta!");
+                }
+            }
+        });
+    });
+});
